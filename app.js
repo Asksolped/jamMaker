31100e3554b9991017b85fd60d3b137dc6072d63
Ask Solheim <solheimen21@gmail.com>
2024-09-01 12:20:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const gandalfJam = document.querySelector("#gandalf-jam");
+
+gandalfJam.addEventListener("click", function addGandalf() {
+  const videos = ["video/gandalfjam.mp4", "video/hobbitjam.mp4"];
+
+  const randomvideo = videos[Math.floor(Math.random() * videos.length)];
+
+  const videoContainer = document.querySelector("#jam-container");
+
+  const existingVideo = videoContainer.querySelector("video");
+  if (existingVideo) {
+    videoContainer.removeChild(existingVideo);
+  }
+
+  var video = document.createElement("video");
+
+  video.setAttribute("autoplay", "true");
+  video.setAttribute("controls", "true");
+  video.setAttribute("width", "600");
+
+  var source = document.createElement("source");
+
+  source.setAttribute("src", randomvideo);
+  source.setAttribute("type", "video/mp4");
+
+  video.appendChild(source);
+
+  videoContainer.appendChild(video);
+});
